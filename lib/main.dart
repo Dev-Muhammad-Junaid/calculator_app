6fd1dee00d21ac68702dc4814733200efab462ed
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-10-14 02:20:26 +0500

    Buttons Grid Style

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  NeumorphicStyle neumorphicStyle = NeumorphicStyle();
+   MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -13,11 +15,8 @@ class MyApp extends StatelessWidget {
     return NeumorphicApp(
       title: "Calculator",
       home: Scaffold(
-        backgroundColor: NeumorphicColors.accent,
-        appBar: NeumorphicAppBar(
-          centerTitle: true,
-          leading: NeumorphicCloseButton(style: NeumorphicStyle(boxShape: NeumorphicBoxShape.stadium(),)),
-        ),
+        appBar: NeumorphicAppBar(leading: NeumorphicCloseButton(onPressed: (){},),),
+        backgroundColor: Colors.amber,
         body: Column(
           children: [
             Neumorphic(
@@ -25,34 +24,46 @@ class MyApp extends StatelessWidget {
               child: Container(
                 height: 100,
                 width: 400,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white,width: 5,)
-                ),
-                child: TextField(cursorColor:Colors.green,controller: TextEditingController(),decoration: InputDecoration(border: InputBorder.none)),
+                decoration: BoxDecoration(border: Border.all(color: Colors.black12,width: 5,)
+                ), child: TextField(cursorColor:Colors.green,controller: TextEditingController(),decoration: InputDecoration(border: InputBorder.none)),
               ),
             ),
-            GridView.count(
-              crossAxisSpacing: 10,
-              crossAxisCount: 3,
-              shrinkWrap: true,
-              children: [
-              NeumorphicButton(
-                style: NeumorphicStyle(shape: NeumorphicShape.concave,boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(50))),
-                provideHapticFeedback: true,
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 5,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: [
+                NeumorphicButton(
 
-                onPressed: (){},child: Text("1",style: TextStyle(fontSize: 40),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("2",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("3",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("4",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("5",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("6",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("7",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("8",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("9",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("0",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text(".",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              NeumorphicButton(onPressed: (){},child: Text("c",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
-              ],
+                  margin: EdgeInsets.all(15),
+                  provideHapticFeedback: true,
+                  onPressed: (){},child: Text("1",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("2",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("3",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("4",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("5",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("6",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("7",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("8",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("9",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("0",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text(".",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                NeumorphicButton(margin: EdgeInsets.all(15),
+                  onPressed: (){},child: Text("c",style: TextStyle(fontSize: 50),textAlign: TextAlign.center,),),
+                ],
+              ),
             )
           ],
         )
